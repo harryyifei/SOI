8233eb75ab22d8d0dee059f0677789e27b94800f
harryyifei <harryyifei@gmail.com>
2019-11-26 13:28:56 +0800

    delect sinks

--- Script/photoshop 2.js
+++
@@ -1,6 +0,0 @@
-let obj = JSON.parse($response.body)
-obj.mobileProfile.profileStatus = 'PROFILE_AVAILABLE'
-obj.mobileProfile.legacyProfile = '{}'
-obj.mobileProfile.relationshipProfile = '{}'
-$done({body: JSON.stringify(obj)})
-//@ImSingee
