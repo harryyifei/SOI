ed6198e554ed7ca39ed2415298cb31eadf80255e
harryyifei <harryyifei@gmail.com>
2021-08-05 14:41:25 +0800

    update

--- Scripts/bilibili_vip2.js
+++
@@ -1,6 +0,0 @@
-let headers = $request.headers;
-headers['Host'] = 'bili.miao.best';
-$done({headers});
-
-let url = $request.url.replace(/.+playurl/, "https:\/\/bili\.miao\.best\/geturl\/maom\/")
-$done({url});
